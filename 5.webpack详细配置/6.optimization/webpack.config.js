f92c8fe9333a88a29f0501a2cf4949b99ab46f78
zhengshaocong <zhengshaocong@beejeen.com>
2021-07-07 19:27:50 +0800

    cc

--- 5.webpack详细配置/6.optimization/webpack.config.js
+++ 5.webpack详细配置/6.optimization/webpack.config.js
@@ -38,6 +38,7 @@ module.exports = {
       maxAsyncRequests: 5, // 按需加载时并行加载文件最大值
       maxInitialRequests: 3, // 入口jis最大并行请求数量
 
+      asdadjaskdhaskdakjd: 21233211113232,
       // name: true, // 可以使用命名规则
       automaticNameDelimiter: '~', // 命名规则 命名链接符
       cacheGroups: { // 命名规则 分割chunk的组
